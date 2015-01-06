b2c22af260d82c0dedc174bea49f6fb69fa1d64c
Erik Rodriguez <nexal.drax@gmail.com>
2015-01-06 18:37:14 +0000

    Update 1.1
    Main class implemented and minor style fixes. Starting to implement the
    GUI.

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,5 +1,13 @@
 package application;
 
+import control.DictionaryCommand;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.*;
+import model.Image;
+import persistence.ImageLoaderFileList;
+import view.ActionListenerFactory;
+
 public class Application {
     
     static final String path = "C:\\Users\\Public\\Pictures\\Sample Pictures";
@@ -9,6 +17,24 @@ public class Application {
     }
 
     private void execute() {
+        final DictionaryCommand commandSet = new DictionaryCommand();
+        ImageLoaderFileList imageLoaderFileList = new ImageLoaderFileList(path);
+        List<Image> list = imageLoaderFileList.load();
+        
+        ActionListenerFactory actionListenerFactory = new ActionListenerFactory() {
+
+            @Override
+            public ActionListener create(final String name) {
+                return new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        commandSet.get(name).execute();
+                    }
+                };
+            }
+        };
+        
     }
     
 }
